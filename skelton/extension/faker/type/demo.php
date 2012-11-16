d85802ee10418cd8c1b1130eebda5f031a6a3a62
Lewis Dyer <signup@icomefromthenet.com>
2012-11-16 11:15:50 +1100

    fixes for issue #18 demo.php out of date

--- skelton/extension/faker/type/demo.php
+++ skelton/extension/faker/type/demo.php
@@ -1,23 +1,42 @@
 <?php
 namespace Faker\Extension\Faker\Type;
 
-use Faker\Components\Faker\TypeInterface;
+use Faker\Components\Faker\Exception as FakerException,
+    Faker\Components\Faker\Type\Type,
+    Faker\Components\Faker\Utilities,
+    Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition,
+    Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
-class Demo implements TypeInterface
+class Demo extends Type
 {
-    
+
+    // -------------------------------------------------------------------------
+
+    /**
+    * Generate a value
+    *
+    * @return string
+    */
     public function generate($rows,$values = array())
     {
-        
+        return null;
     }
     
     
-    public function getId()
+   
+    // -------------------------------------------------------------------------
+    
+    /**
+    * Generates the configuration tree builder.
+    *
+    * @return \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition
+    */
+    public function getConfigExtension(ArrayNodeDefinition $rootNode)
     {
-        
+        return $rootNode;
+            
     }
     
     
+    // -------------------------------------------------------------------------
 }
-
-/* End of File */
