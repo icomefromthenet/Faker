df097961e808411c4c2d8f7038b6a57e66ec931a
Lewis Dyer <signup@icomefromthenet.com>
2013-02-11 14:55:12 +1100

    finished porting selectors to common engine

--- src/Faker/Components/Engine/Common/Selector/PickSelector.php
+++ src/Faker/Components/Engine/Common/Selector/PickSelector.php
@@ -1,10 +1,14 @@
 <?php
 namespace Faker\Components\Engine\Common\Selector;
 
-use Faker\Components\Engine\Common\Type;
+use Faker\Components\Engine\Common\Type\Type;
+use Faker\Components\Engine\Common\PositionManager;
+use Faker\Components\Engine\EngineException;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
 
 /**
- * Picks between two alternatives using probability as weight measurement
+ * Picks between two alternatives using weighted probability to skew the number
  *
  * @author Lewis Dyer <getintouch@icomefromthenet.com>
  * @since 1.0.4
@@ -16,9 +20,11 @@ class PickSelector extends Type
    
     public function generate($rows,$values = array())
     {
-        $prob = $this->getOption('probabiliy');
+        $prob      = $this->getOption('probability');
+        $generated = round($this->getGenerator()->generate(0,100));
+        $rounded   = round($prob * 100);
         
-        return (round($this->getGenerator()->generate(0,100)) <= ($prob * 100)) ? 0 : 1;
+        return $generated <=  $rounded ? 1 : 0;
     }
     
     
@@ -38,7 +44,7 @@ class PickSelector extends Type
                 ->example('0.3 | 0.4 | 0.5')
                 ->validate()
                     ->ifTrue(function($v){
-                        return !is_numeric($v) || ($v > 0 || $v < 1);
+                        return !is_numeric($v) || !($v > 0 && $v < 1);
                     })
                     ->then(function($v){
                         throw new EngineException('PickSelector::Probability must be between 0 and  1');
